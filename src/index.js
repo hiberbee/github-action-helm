d2eceb2ab7ce1a247fc6ffdf1186395e7cdb22d6
Vlad Volkov <vlad.volkov@bndigital.co>
2023-03-30 09:01:26 +0300

    Update index.js
    Signed-off-by: Vlad Volkov <vlad.volkov@bndigital.co>

--- src/index.js
+++ src/index.js
@@ -7825,6 +7825,7 @@ PERFORMANCE OF THIS SOFTWARE.
       var __awaiter =
         (this && this.__awaiter) ||
         function (thisArg, _arguments, P, generator) {
+          function adopt(value) {
             return value instanceof P
               ? value
               : new P(function (resolve) {
